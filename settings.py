6feec649542bc5b71bdff53e0d6f076a570ba667
Josh Arnold <josharnold997@gmail.com>
2018-12-19 12:19:55 -1000

    Updated settings to use 512

--- settings.py
+++ settings.py
@@ -1,8 +1,8 @@
 config = {
-	"num_epochs":5,
+	"num_epochs":7,
 	"batch_size":128,
 	"word_dimension":1000, # The dimensionality of word embeddings
-	"image_dimension":4096, # input image dimension : 4096 for VGG
+	"image_dimension":512, # input image dimension : 4096 for VGG, 512 resnet
 	"model_dimension":1000, # The dimension of the embedding space,
 	"learning_rate":0.01,
 	"display_freq":50, # how often to display loss : 1 = every batch, 2 = every second batch etc...
